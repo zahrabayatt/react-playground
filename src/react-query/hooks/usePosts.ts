1b5458d9c1345328382ed2b74dda81a4388ea38a
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-09-28 19:25:50 +0330

    create post list and todo list

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = (userId: number | undefined) => {
+  const fetchPosts = () =>
+    axios
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          userId,
+        },
+      })
+      .then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryFn: fetchPosts,
+    staleTime: 1 * 60 * 1000, //1m
+  });
+};
+
+export default usePosts;
