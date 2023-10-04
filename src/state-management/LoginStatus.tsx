9410cfe8c0e1d23fc1338791d5903450333bc473
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-10-04 18:28:56 +0330

    Add LoginStatus component

---
+++ src/state-management/LoginStatus.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const LoginStatus = () => {
+  const [user, setUser] = useState("");
+  if (user) {
+    return (
+      <>
+        <div>
+          <span className="mx-2">{user}</span>
+          <a onClick={() => setUser("")} href="#">
+            Logout
+          </a>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <a onClick={() => setUser("zahra.bayat")} href="#">
+        Login
+      </a>
+    </div>
+  );
+};
+
+export default LoginStatus;
