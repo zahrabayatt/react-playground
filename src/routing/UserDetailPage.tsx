e78cca847a88d21b76cab5d8284c6e973bf3ac0f
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-10-05 15:50:07 +0330

    Getting data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,18 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log(params); //{id: '1'}
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  // setSearchParams has side effect so use it inside useEffect or event handler
+  console.log(searchParams); // name=Alice&age=25
+  console.log(searchParams.get("name")); //Alice
+
+  const location = useLocation();
+  console.log(location);
+  // {pathname: '/users/1', search: 'name=Alice&age=25', hash: '', key: default, state: null}
+
   return <p>User</p>;
 };
 
